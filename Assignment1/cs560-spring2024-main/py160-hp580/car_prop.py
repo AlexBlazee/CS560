0bdd7482a71cf5b7dda11011a9cfb1b819681a50
Axel Blaze <pradeepy121@gmail.com>
2024-04-29 09:42:04 -0400

    files
    Data collection

--- Assignment1/cs560-spring2024-main/py160-hp580/car_prop.py
+++ Assignment1/cs560-spring2024-main/py160-hp580/car_prop.py
@@ -2,6 +2,7 @@ import numpy as np
 import argparse
 from geometry import * 
 from threejs_group import *
+import copy
 
 class CarRobot:
     def __init__(self, q0 , actuation_noise, odometry_noise , observation_noise, viz_out , landmarks = None, L=1.5, dt=0.1 ):
@@ -35,7 +36,7 @@ class CarRobot:
         x_dot = v * np.cos(theta)
         y_dot = v * np.sin(theta)
         theta_dot = (v / self.L) * np.tan(phi)
-        # theta_dot = (theta_dot + np.pi) % (2 * np.pi) - np.pi
+        theta_dot = (theta_dot + np.pi) % (2 * np.pi) - np.pi
         return np.array([x_dot, y_dot, theta_dot]),[v,phi]
 
     def odometry_measurement(self, u_e):
@@ -117,6 +118,41 @@ class CarRobot:
         self.viz_out.add_animation(cube , car_trajectory)
         self.viz_out.add_line(line_trajectory , black)
         return self.viz_out    
+
+
+    def visualize_given_trajectory_name(self, trajectory , name):
+        yellow = "0xFFFF00"
+        car_trajectory = []
+        for t in range(len(trajectory)):
+            x,y,theta = trajectory[t]
+            quat = self.rotate_z_quaternion(theta=theta) 
+            car_trajectory.append([t, [x,y,0.5] , quat , yellow ])
+        
+        cube = box(name, 2,1,1, car_trajectory[0][1], car_trajectory[0][2])
+        self.viz_out.add_animation(cube , car_trajectory)
+        return self.viz_out    
+
+    def visualize_linepath_color(self, trajectory_path ,  color):  
+        trajectory_path = copy.deepcopy(trajectory_path)
+        trajectory_path[:,2] = 0.5
+        self.viz_out.add_line(trajectory_path, color) 
+        return self.viz_out 
+
+    def visualize_landmark_observation(self, landmark_data , trajectory):
+        gray = "#808080"
+        landmark_count = int(len(landmark_data[0])/2)
+        observation_per_step = [[] for _ in range(landmark_count)]
+        for t, observation in enumerate(landmark_data):
+            for j in range(0,landmark_count):
+                rel_dist,rel_angle = observation[j*2],observation[(2*j)+1]
+                pos_x,pos_y,pos_theta = trajectory[t]
+                l_pos_x = pos_x + (rel_dist * np.cos(rel_angle + pos_theta))
+                l_pos_y = pos_y + (rel_dist * np.sin(rel_angle + pos_theta))
+                observation_per_step[j].append([t , [l_pos_x,l_pos_y, 0.5] , [1,0,0,0] , gray])
+        for j in range(landmark_count):
+            geom = sphere('lm_'+str(j) , 0.5 , observation_per_step[j][0][1], observation_per_step[j][0][2] )
+            self.viz_out.add_animation(geom , observation_per_step[j])
+        return self.viz_out
 
 if __name__ == "__main__":
 
